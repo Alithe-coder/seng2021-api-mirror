53ca805f29ee0aa96b358cf00a9a4fa58dbb7fa4
Ali Mohemmed <z5715193@ad.unsw.edu.au>
2026-03-09 18:16:40 +1100

    improved documentation for boiler plate and skeleton code + introduced controller, index, router files

--- src/controllers/orderController.ts
+++ src/controllers/orderController.ts
@@ -1,7 +1,11 @@
 
+// this is a Controller file which recieves the requests from Router file, and it responsible
+// for executing code depending on what it receives. its essentially the backend functionality
+// for api endpoints
+
 import express from 'express'
 
-// POST
+// create an order
 export const createOrder = (req: express.Request, res: express.Response) => {
     res.status(201).json({ message: "TESTING: Create order" });
 };
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,22 @@
 
+// index.ts is the main file which will run everytime the Node server starts. ALl 
+
 import express from 'express';
 import orderRoutes from './routes/orderRoutes.ts';
 
 const app = express();
 const port = 3000;
 
-// we need to use /api/v1/orders everytiume
+// we need to use /api/v1/orders everytime we are to do with orders
 app.use('/api/v1/orders', orderRoutes);
 
+// health check to see how the server is going 
 app.get('/api/v1/health', (req, res) => {
-    res.status(200).json({status : "Recieved"})
+    res.status(200).json({status : "healthy 🫀   "})
 });
 
+
+// when server is running
 app.listen(port, () => {
     console.log(`listening...`);
 });
