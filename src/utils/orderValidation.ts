2871adda6f259ac12ab80358d3a1338163ba480c
Ali Mohemmed <z5715193@ad.unsw.edu.au>
2026-03-12 16:45:46 +1100

    fixed npx tsc errors

--- src/utils/orderValidation.ts
+++ src/utils/orderValidation.ts
@@ -3,8 +3,6 @@
 // this file is a backend data validation - it checks whether
 // the input data isnt garbage. we can have multiple validaitionErrors
 
-
-
 export interface validationError {
     field: string;
     issue: string;
@@ -12,37 +10,40 @@ export interface validationError {
 
 // helper functions
 // check if any values are missing from request
-function requireField(value: unknown, fieldName: string, errors: validationError[]) {
+function requireField(value: any, fieldName: string, errors: validationError[]) {
     if (!value) {
         errors.push({ field: fieldName, issue: 'required' });
     }
 }
-function checkDate(dateString: unknown, fieldName: string, errors: validationError[] ) {
+function checkDate(dateString: any, fieldName: string, errors: validationError[] ) {
     // use some simple regex; 4 digits - 2 digits - 2 digts
     if (dateString && !/^\d{4}-\d{2}-\d{2}$/.test(dateString)) {
         errors.push({ field: fieldName, issue: 'format must be YYYY-MM-DD' });
     }
 };
 
-function checkAddress(address: unknown, prefix: string, errors: validationError[]) {
+function checkAddress(address: any, prefix: string, errors: validationError[]) {
     if (!address) {
-        errors.push({ field: fieldName, issue: 'required' });
+        errors.push({ field: prefix, issue: 'required' });
         return; // if no address, just stop
     }
     // make sure all the address info is there
-    checkRequired(address.line1, `${prefix}.line1`, errors);
-    checkRequired(address.city, `${prefix}.city`, errors);
-    checkRequired(address.postcode, `${prefix}.postcode`, errors);
-    checkRequired(address.countryCode, `${prefix}.countryCode`, errors);
+    requireField(address.line1, `${prefix}.line1`, errors);
+    requireField(address.city, `${prefix}.city`, errors);
+    requireField(address.postcode, `${prefix}.postcode`, errors);
+    requireField(address.countryCode, `${prefix}.countryCode`, errors);
 }
 
 // validate data for POST
-export function validateCreateOrder (body: unknown) : validationError[] {
+export function validateCreateOrder (body: any) : validationError[] {
     const errors : validationError[] = [];
 
     // 1. check issue date format
     // 2. check buyer
     // 3. check seller check lines
+
+    // return a list of errors
+    return errors;
 }
 
 // validate data for PUT
