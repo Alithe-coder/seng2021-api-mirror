ff2c5acf723eaf642b8d634813d144090f4bfd2c
Ali Mohemmed <z5715193@ad.unsw.edu.au>
2026-03-12 11:55:50 +1100

    Finally setup the connection to the database, so now API can communicate

---
+++ src/db.ts
@@ -0,0 +1,14 @@
+
+import 'dotenv/config';
+console.log('DATABASE_URL:', process.env.DATABASE_URL);
+
+import { PrismaClient } from "@prisma/client";
+
+import { PrismaPg } from "@prisma/adapter-pg";
+
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+export const prisma = new PrismaClient({ adapter });
