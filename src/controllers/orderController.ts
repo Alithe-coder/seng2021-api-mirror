5b6f72436b9847da2727d7e0e09f92d3559a329c
Joel Christudoss <z5693524@student.unsw.edu.au>
2026-03-13 19:30:10 +1100

    Added code, to POST seller party to the database

--- src/controllers/orderController.ts
+++ src/controllers/orderController.ts
@@ -13,43 +13,49 @@ export const createOrder = async (req: express.Request, res: express.Response) =
     const dataReceived = req.body;
     console.log(dataReceived);
 
-    const newOrder = await prisma.order.create({
-        data: {
-            buyerName: req.body.buyerName,
-            item: req.body.item,
-            quantity: req.body.quantity
-        }
-    })
-
-    res.status(201).json(newOrder);
+    res.status(200).json({message: "not avaialbe"});
 };
 
 export const createSellerParty = async (req: express.Request, res: express.Response) => {
     const dataReceived = req.body;
     console.log(dataReceived);
+/*
+*/
 
-    const newSeller = await prisma.SellerSupplierParty.create({
-        data: {
-            address: {
-                street: req.body.streetNo,
+    const newSeller = await prisma.sellerCustomerParty.create({
+    data: {
+        address: {
+            create: {
+                street: Number(req.body.streetNo),
                 streetName: req.body.streetName,
-                postCode: req.body.postCode,
+                postCode: Number(req.body.postCode),
                 suburbName: req.body.suburbName,
                 stateName: req.body.stateName,
-            },
-            contact: {
-                phoneNo: req.body.phoneNo,
-                telefax: req.body.telefax,
+            }
+        },
+        contact: {
+            create: {
+                phoneNo: Number(req.body.phoneNo),
+                telefax: Number(req.body.telefax),
                 email: req.body.email
-            },
-            person: {
+            }
+        },
+        person: {
+            create: {
                 firstName: req.body.firstName,
                 surname: req.body.surname,
                 jobTitle: req.body.jobTitle
             }
         }
-    })
-    res.status(201).json(newSeller);
+    },
+    include: {
+        address: true,
+        contact: true,
+        person: true
+    }
+});
+
+    res.status(201).json("newPerson");
 }
 
 // GET /api/v1/orders - List orders using filters
--- src/db.ts
+++ src/db.ts
@@ -2,13 +2,13 @@
 import 'dotenv/config';
 console.log('DATABASE_URL:', process.env.DATABASE_URL);
 
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "./generated/prisma/client.js";
 
 import { PrismaPg } from "@prisma/adapter-pg";
 
 
 const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
+  connectionString: "postgresql://neondb_owner:npg_QOSDKi63ZJUR@ep-falling-boat-a7x69krr-pooler.ap-southeast-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
 });
 
 export const prisma = new PrismaClient({ adapter });
